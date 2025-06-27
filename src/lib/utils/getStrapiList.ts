385e8a0184842b818977682f2a755251a1e5e5b2
Jhona <jhonattanloli@gmail.com>
2025-06-27 18:58:05 -0300

    try change cache config

--- src/lib/utils/getStrapiList.ts
+++ src/lib/utils/getStrapiList.ts
@@ -2,6 +2,7 @@ export async function getStrapiList<T>(url: string, queryParams?: string) {
   const response = await fetch(
     `${process.env['NEXT_PUBLIC_STRAPI_URL']}/${url}?${queryParams}`,
     {
+      next: { revalidate: 60 },
       headers: {
         Authorization: `Bearer ${process.env['NEXT_PUBLIC_STRAPI_TOKEN']}`,
       },
