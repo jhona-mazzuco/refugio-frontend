20a85992e3e54744f2bd7eaab311aae7cf963e2f
Jhona <jhonattanloli@gmail.com>
2025-09-19 20:34:04 -0300

    add sort release dates

--- src/app/calendario/page.tsx
+++ src/app/calendario/page.tsx
@@ -1,6 +1,6 @@
 import { releasesQuery } from '@/lib/queries/releasesQuery';
 import { Release } from '@/lib/models/Release';
-import { format, parseISO } from 'date-fns';
+import { endOfYear, format, isAfter, parseISO, toDate } from 'date-fns';
 import { ReleaseTable } from '@/app/ui/ReleaseTable';
 import { pt } from 'date-fns/locale';
 import { getStrapiList } from '@/lib/utils/getStrapiList';
@@ -8,12 +8,21 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Calendário - Refúgio Gamer',
-}
+};
 
 export default async function ReleasePage() {
   const response = await getStrapiList<Release>('calendars', releasesQuery());
   const dataMap = new Map<string, Release[]>();
-  for (const release of response.data) {
+  const sortedData = response.data.sort((a, b) => {
+    const notHaveReleaseDate = b.dateFormat === 'yyyy';
+    if (notHaveReleaseDate) {
+      return isAfter(endOfYear(b.releaseDate), toDate(a.releaseDate)) ? -1 : 1;
+    }
+
+    return isAfter(toDate(b.releaseDate), toDate(a.releaseDate)) ? -1 : 1;
+  });
+
+  for (const release of sortedData) {
     const hasReleaseDate = release.dateFormat !== 'yyyy';
     const releaseDateISO = parseISO(release.releaseDate);
     const key = format(releaseDateISO, hasReleaseDate ? 'MMMM/yy' : 'yyyy', {
